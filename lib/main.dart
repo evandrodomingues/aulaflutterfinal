4f0b687c1ff7e622559f262b7271e37cf30328a1
Evandro Domingues <evsdomingues@gmail.com>
2022-01-18 21:02:27 -0300

    Projeto Flutter Aula. Aula 1 - Arquivos iniciais

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,23 @@
+import 'package:aulaflutterfinal/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
 void main() {
-  
+  runApp(const MeuAplicativo());
+}
+
+class MeuAplicativo extends StatelessWidget {
+  const MeuAplicativo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Brasileirão',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomePage(),
+    );
+  }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Olá pessoal',
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}
